dc341f3dae7b959eaea0f68442b25a9bdb185969
sukru <sukru.hasta@ceng.deu.edu.tr>
2021-04-23 12:53:59 +0300

    fragments

--- app/src/main/java/com/example/mobilhotelqr/HomeActivity.java
+++ app/src/main/java/com/example/mobilhotelqr/HomeActivity.java
@@ -1,19 +1,56 @@
 package com.example.mobilhotelqr;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class HomeActivity extends AppCompatActivity {
+
+
+    private BottomNavigationView bottom_navigation;
+    private Fragment tempFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        //sukru deneme
-
         String mesaj = getIntent().getStringExtra("mesaj");
         Toast.makeText(this,mesaj,Toast.LENGTH_SHORT).show();
+
+        bottom_navigation=findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_tutucu,new fragment_first()).commit();
+
+        bottom_navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId()==R.id.dashboard){
+                    Toast.makeText(getApplicationContext(), "dashboard",Toast.LENGTH_SHORT).show();
+                    tempFragment=new fragment_first();
+
+                }
+                if(item.getItemId()==R.id.order){
+                    Toast.makeText(getApplicationContext(), "order",Toast.LENGTH_SHORT).show();
+                    tempFragment=new fragment_second();
+
+                }
+                if(item.getItemId()==R.id.tripadvisor){
+                    Toast.makeText(getApplicationContext(), "tripAdvisor",Toast.LENGTH_SHORT).show();
+                    tempFragment=new fragment_third();
+                }
+
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_tutucu,tempFragment).commit();
+
+                return true;
+            }
+        });
     }
+
 }
