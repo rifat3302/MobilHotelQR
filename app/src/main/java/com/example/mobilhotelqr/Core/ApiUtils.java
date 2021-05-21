5e22cc9de12e3bd65c7c3fc55a902e7ff6678c13
Rifat Gürbüz <gurbuzrifat3302@gmail.comm>
2021-05-21 21:22:07 +0300

    Last order finished

--- app/src/main/java/com/example/mobilhotelqr/Core/ApiUtils.java
+++ app/src/main/java/com/example/mobilhotelqr/Core/ApiUtils.java
@@ -2,13 +2,18 @@ package com.example.mobilhotelqr.Core;
 
 public class ApiUtils {
 
-    public static final String BASE_URL="https://seocuyuz.online/mobdet/api/";
+    public static final String BASE_URL="https://www.burak.social/api/";
+    //public static final String BASE_URL="https://seocuyuz.online/mobdet/api/";
 
     public static RetrofitProcess getOccupancy(){
         return  RetrofitClient.getClient(BASE_URL).create(RetrofitProcess.class);
     }
 
     public static RetrofitProcess getAllMenu(){
+        return  RetrofitClient.getClient(BASE_URL).create(RetrofitProcess.class);
+    }
+
+    public static RetrofitProcess saveOrder(){
         return  RetrofitClient.getClient(BASE_URL).create(RetrofitProcess.class);
     }
 }
